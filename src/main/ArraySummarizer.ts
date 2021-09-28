f1fa7cfde46fb92674d38f3d812f91f8ada39403
Zahiriddin Adhamov <zahadhamov@gmail.com>
2021-09-28 14:30:27 +0300

    implemented funcs, homework done!

--- src/main/ArraySummarizer.ts
+++ src/main/ArraySummarizer.ts
@@ -8,7 +8,11 @@ export class ArraySummarizer {
      * @return Sum of the array items.
      */
     sumArrayItems(array: number[]): number {
-        throw new Error("Not implemented!");
+        let sum = 0;
+        for (let i = 0; i < array.length; i += 1){
+            sum += array[i];
+        }
+        return sum;
     }
 
 }
