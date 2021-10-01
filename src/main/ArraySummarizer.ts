2d5071ef87940a4fd1e38c30ce516da3c9cef7c0
AlinaOkh <rumalina12@yandex.ru>
2021-10-01 13:22:05 +0300

    Home work1 in progress

--- src/main/ArraySummarizer.ts
+++ src/main/ArraySummarizer.ts
@@ -1,6 +1,5 @@
 export class ArraySummarizer {
-
-    /**
+     /**
      * Summarize all elements of the array.
      *
      * @param array Array of numbers.
@@ -8,7 +7,12 @@ export class ArraySummarizer {
      * @return Sum of the array items.
      */
     sumArrayItems(array: number[]): number {
-        throw new Error("Not implemented!");
+        var sum = 0;
+        for (var i=0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        return sum;
     }
 
 }
